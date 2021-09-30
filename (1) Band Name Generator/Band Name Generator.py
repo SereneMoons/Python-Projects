076da85f863cd521297d9fd00052a2d01d4d8e67
SereneMoons <86084846+SereneMoons@users.noreply.github.com>
2021-09-30 12:07:06 -0400

    Add files via upload

---
+++ (1) Band Name Generator/Band Name Generator.py
@@ -0,0 +1,9 @@
+#Beginning - Program 1 (Band Name Generator)
+#Inputs and Variables
+print("Welcome to Band Name Generator!")
+city = input("What city did you grow up in?\n")
+name_pet = input("What is the name of your pet?\n")
+
+#Final printing
+print("Your band name is " + city + " " + name_pet)
+#End of program
